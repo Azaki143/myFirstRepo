f9783c256cb096a24a5eaef0219a7d2020eef2ed
Bibi Nadia Hussainkhail <bibinadiahussainkhail@Bibis-Air.attlocal.net>
2023-02-21 17:27:16 -0800

    This is the new changes

---
+++ src/Test/GitPracticeTwo.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class GitPracticeTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Thi is for push practice");
+		System.out.println("This is for pull practice");
+	}
+
+}
--- src/Test/UniteTest.java
+++ src/Test/UniteTest.java
@@ -3,14 +3,16 @@ package Test;
 public class UniteTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		int a = 10;
-		int b = 15; 
-		int c = a+b; 
 		
-		System.out.println(c);
-		System.out.println("ali is 21 years old");
+
+		
+		
+		System.out.println(testOne(10, 5));
 	}
 
+	public static int testOne (int a, int b) {
+		return a-b; 
+	}
+	
+	
 }
